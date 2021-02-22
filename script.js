a1be893392055959bc09f2c6c70b408cb8929ae6
Rajab <mohamaragab0@gmail.com>
2021-02-22 14:18:33 +0200

    ParticlesJs

---
+++ script.js
@@ -0,0 +1,13 @@
+/** 
+ * =========================
+ *    start Particles Js
+ * =========================
+ */
+    particlesJS.load('Description', "./js/vendors/Particles.json", function () {
+        console.log('Loading')
+    })
+/** 
+ * =========================
+ *    End Particles Js
+ * =========================
+ */
