e306932e0455073b8e60ab83f3975730f6bff86b
Ragab Muhamed Ragab <64719825+ragabmoh2ooo@users.noreply.github.com>
2021-08-15 19:48:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,7 @@
  *    End Particles Js
  * =========================
  */
+/*
+Syntax Of Particles 
+particlesJs.load('#ID_Of_Element','Path Of Particles.json(file Json)',function(){console.log('load')});
+*/
